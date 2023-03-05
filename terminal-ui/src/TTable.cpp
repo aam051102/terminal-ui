a59ffed139a7afd0b04eb596ff0bfc4af9b7c815
MadCreativity <ahlgreenmadsen@gmail.com>
2023-03-05 14:56:26 +0100

    Add initial dividers.

--- terminal-ui/src/TTable.cpp
+++ terminal-ui/src/TTable.cpp
@@ -4,8 +4,8 @@
 
 namespace TUI {
     const std::unordered_map<ETTableBorderStyle, std::vector<wchar_t>> borderCharMap = {
-        { ETTableBorderStyle::SINGLE, {L'\u250c', L'\u2500', L'\u2510', L'\u2502', L'\u2518', L'\u2500', L'\u2514', L'\u2502', ' '}},
-        { ETTableBorderStyle::DOUBLE, {L'\u2554', L'\u2550', L'\u2557', L'\u2551', L'\u255d', L'\u2550', L'\u255a', L'\u2551', ' '}},
+        { ETTableBorderStyle::SINGLE, {L'\u250c', L'\u2500', L'\u2510', L'\u2502', L'\u2518', L'\u2500', L'\u2514', L'\u2502', ' ', L'\u2502', L'\u2500'}},
+        { ETTableBorderStyle::DOUBLE, {L'\u2554', L'\u2550', L'\u2557', L'\u2551', L'\u255d', L'\u2550', L'\u255a', L'\u2551', ' ', L'\u2551', L'\u2550'}},
     };
 
     TTable::TTable() {
@@ -42,8 +42,8 @@ namespace TUI {
             totalContentWidth += contentWidths[i];
         }
 
-        const size_t width = 2 + totalContentWidth + cols * (this->padding.left + this->padding.right);
-        const size_t height = 2 + this->rows.size() + this->rows.size() * (this->padding.top + this->padding.bottom);
+        const size_t width = 1 + totalContentWidth + cols * (this->padding.left + this->padding.right + 1);
+        const size_t height = 1 + this->rows.size() + this->rows.size() * (this->padding.top + this->padding.bottom + 1);
        
         // Render
         std::wstring out = L"";
@@ -51,14 +51,14 @@ namespace TUI {
         const std::vector<wchar_t>* borderCharSet = &borderCharMap.at(this->borderStyle);
 
         for (size_t y = 0; y < height; y++) {
-            size_t row = std::floor((y - 1 - this->padding.top) / (float)(this->padding.top + this->padding.bottom + 1));
-            bool isContentRow = y >= 1 + this->padding.top && ((y - 1 - this->padding.top) % (this->padding.top + this->padding.bottom + 1)) == 0 && y != height - 1;
+            size_t row = std::floor((y - 1 - this->padding.top) / (float)(this->padding.top + this->padding.bottom + 1 + 1));
+            bool isContentRow = y >= 1 + this->padding.top && ((y - 1 - this->padding.top) % (this->padding.top + this->padding.bottom + 1 + 1)) == 0 && y != height - 1;
 
             size_t col = 0;
             size_t passedContentWidth = 0;
 
             for (size_t x = 0; x < width; x++) {
-                size_t colStart = 1 + passedContentWidth + (col * (this->padding.left + this->padding.right)) + this->padding.left;
+                size_t colStart = 1 + passedContentWidth + (col * (this->padding.left + this->padding.right + 1)) + this->padding.left;
                 size_t offset = x - colStart;
                 bool isContentCol = x >= 1 + this->padding.left && x < width - 1 && offset < contentWidths[col];
 
@@ -67,7 +67,7 @@ namespace TUI {
                     col++;
                 }
 
-                if (isContentRow && isContentCol && this->rows[row].cells[col].content.size() > offset) {
+                if (isContentRow && isContentCol && this->rows[row].cells.size() > col && this->rows[row].cells[col].content.size() > offset) {
                     out += this->rows[row].cells[col].content[offset];
                 }
                 else {
@@ -100,6 +100,12 @@ namespace TUI {
                     }
                     else if (x == width - 1) {
                         charIndex = 3;
+                    }
+                    else if (x == colStart - this->padding.left - 1) {
+                        charIndex = 9;
+                    }
+                    else if (y % (this->padding.top + this->padding.bottom + 1 + 1) == 0) {
+                        charIndex = 10;
                     }
 
                     out += (*borderCharSet)[charIndex];
