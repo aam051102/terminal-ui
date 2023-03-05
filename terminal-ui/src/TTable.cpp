5a67190d9c5be648b72d17cba3f388cee3c5f6ab
MadCreativity <ahlgreenmadsen@gmail.com>
2023-03-05 15:23:22 +0100

    Fix dividors.

--- terminal-ui/src/TTable.cpp
+++ terminal-ui/src/TTable.cpp
@@ -4,13 +4,13 @@
 
 namespace TUI {
     const std::unordered_map<ETTableBorderStyle, std::vector<wchar_t>> borderCharMap = {
-        { ETTableBorderStyle::SINGLE, {L'\u250c', L'\u2500', L'\u2510', L'\u2502', L'\u2518', L'\u2500', L'\u2514', L'\u2502', ' ', L'\u2502', L'\u2500'}},
-        { ETTableBorderStyle::DOUBLE, {L'\u2554', L'\u2550', L'\u2557', L'\u2551', L'\u255d', L'\u2550', L'\u255a', L'\u2551', ' ', L'\u2551', L'\u2550'}},
+        { ETTableBorderStyle::SINGLE, {L'\u250c', L'\u2500', L'\u2510', L'\u2502', L'\u2518', L'\u2500', L'\u2514', L'\u2502', ' ', L'\u2502', L'\u2500', L'\u253c', L'\u251c', L'\u2524', L'\u252c', L'\u2534'}},
+        { ETTableBorderStyle::DOUBLE, {L'\u2554', L'\u2550', L'\u2557', L'\u2551', L'\u255d', L'\u2550', L'\u255a', L'\u2551', ' ', L'\u2551', L'\u2550', L'\u256c', L'\u2560', L'\u2563', L'\u2566', L'\u2569'}},
     };
 
     TTable::TTable() {
         this->borderStyle = ETTableBorderStyle::SINGLE;
-        this->padding = Rect(0, 0, 1, 1);
+        this->padding = Rect(1, 1, 2, 2);
         this->rows = {};
     }
 
@@ -60,7 +60,7 @@ namespace TUI {
             for (size_t x = 0; x < width; x++) {
                 size_t colStart = 1 + passedContentWidth + (col * (this->padding.left + this->padding.right + 1)) + this->padding.left;
                 size_t offset = x - colStart;
-                bool isContentCol = x >= 1 + this->padding.left && x < width - 1 && offset < contentWidths[col];
+                bool isContentCol = x >= 1 + this->padding.left && x < width - 1 && col < cols && offset < contentWidths[col];
 
                 if (col < cols && offset == contentWidths[col]) {
                     passedContentWidth += contentWidths[col];
@@ -72,6 +72,8 @@ namespace TUI {
                 }
                 else {
                     size_t charIndex = 8;
+                    bool isMergedVertically = y % (this->padding.top + this->padding.bottom + 1 + 1) == 0;
+                    bool isMergedHorizontally = (x - passedContentWidth) == (col * (this->padding.left + this->padding.right + 1));
 
                     if (y == 0) {
                         if (x == 0) {
@@ -79,6 +81,9 @@ namespace TUI {
                         }
                         else if (x == width - 1) {
                             charIndex = 2;
+                        }
+                        else if (isMergedHorizontally) {
+                            charIndex = 14;
                         }
                         else {
                             charIndex = 1;
@@ -91,20 +96,35 @@ namespace TUI {
                         else if (x == width - 1) {
                             charIndex = 4;
                         }
+                        else if (isMergedHorizontally) {
+                            charIndex = 15;
+                        }
                         else {
                             charIndex = 5;
                         }
                     }
                     else if (x == 0) {
                         charIndex = 7;
+
+                        if (isMergedVertically) {
+                            charIndex = 12;
+                        }
                     }
                     else if (x == width - 1) {
                         charIndex = 3;
+
+                        if (isMergedVertically) {
+                            charIndex = 13;
+                        }
                     }
-                    else if (x == colStart - this->padding.left - 1) {
+                    else if (isMergedHorizontally) {
                         charIndex = 9;
+
+                        if (isMergedVertically) {
+                            charIndex = 11;
+                        }
                     }
-                    else if (y % (this->padding.top + this->padding.bottom + 1 + 1) == 0) {
+                    else if (isMergedVertically) {
                         charIndex = 10;
                     }
 
