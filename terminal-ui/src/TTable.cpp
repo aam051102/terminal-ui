662034de568784c1710c4a099dc15616f1ccdea7
MadCreativity <ahlgreenmadsen@gmail.com>
2023-03-17 08:51:26 +0100

    Optimize table rendering.

--- terminal-ui/src/TTable.cpp
+++ terminal-ui/src/TTable.cpp
@@ -24,14 +24,14 @@ namespace TUI {
         std::vector<size_t> contentWidths;
 
         for (size_t i = 0, l = this->rows.size(); i < l; i++) {
-            TTableRow row = this->rows[i];
+            const TTableRow& row = this->rows[i];
 
             if (contentWidths.size() < row.cells.size()) {
                 contentWidths.resize(row.cells.size());
             }
 
             for (size_t j = 0, l2 = row.cells.size(); j < l2; j++) {
-                TTableCell cell = row.cells[j];
+                const TTableCell& cell = row.cells[j];
 
                 if (cell.content.length() > contentWidths[j]) {
                     contentWidths[j] = cell.content.length();
@@ -39,106 +39,87 @@ namespace TUI {
             }
         }
 
-        const size_t cols = contentWidths.size();
-        
-        size_t totalContentWidth = 0;
+        const size_t colCount = contentWidths.size();
+        const std::vector<wchar_t>& borderCharSet = *(&borderCharMap.at(this->borderStyle));
 
-        for (size_t i = 0; i < cols; i++) {
-            totalContentWidth += contentWidths[i];
+        // Prerender
+        std::wstring prerenderedBlank;
+        std::wstring prerenderedTop;
+        std::wstring prerenderedDivider;
+        std::wstring prerenderedBottom;
+
+        std::wstring prerenderedPaddingTop;
+        std::wstring prerenderedPaddingBottom;
+
+        prerenderedTop += borderCharSet[0];
+        prerenderedDivider += borderCharSet[12];
+        prerenderedBottom += borderCharSet[6];
+
+        for (size_t i = 0; i < colCount; i++) {
+            prerenderedBlank += borderCharSet[7];
+            
+            if (i != 0) {
+                prerenderedTop += borderCharSet[14];
+                prerenderedDivider += borderCharSet[11];
+                prerenderedBottom += borderCharSet[15];
+            }
+
+            for (size_t j = 0; j < contentWidths[i] + this->padding.left + this->padding.right; j++) {
+                prerenderedBlank += borderCharSet[8];
+                prerenderedTop += borderCharSet[1];
+                prerenderedDivider += borderCharSet[10];
+                prerenderedBottom += borderCharSet[5];
+            }
         }
 
-        const size_t width = 1 + totalContentWidth + cols * (this->padding.left + this->padding.right + 1);
-        const size_t height = 1 + this->rows.size() + this->rows.size() * (this->padding.top + this->padding.bottom + 1);
-       
+        prerenderedBlank += borderCharSet[3];
+        prerenderedTop += borderCharSet[2];
+        prerenderedDivider += borderCharSet[13];
+        prerenderedBottom += borderCharSet[4];
+
+        for (size_t i = 0; i < this->padding.top; i++) {
+            prerenderedPaddingTop += prerenderedBlank;
+            if (i != this->padding.top - 1) prerenderedPaddingTop += L"\n";
+        }
+        
+        for (size_t i = 0; i < this->padding.bottom; i++) {
+            prerenderedPaddingBottom += prerenderedBlank + L"\n";
+            if (i != this->padding.bottom - 1) prerenderedPaddingBottom += L"\n";
+        }
+
         // Render
-        std::wstring out = L"";
+        std::wstring out;
 
-        const std::vector<wchar_t>* borderCharSet = &borderCharMap.at(this->borderStyle);
+        out += prerenderedTop + L"\n";
 
-        for (size_t y = 0; y < height; y++) {
-            size_t row = std::floor((y - 1 - this->padding.top) / (float)(this->padding.top + this->padding.bottom + 1 + 1));
-            bool isContentRow = y >= 1 + this->padding.top && ((y - 1 - this->padding.top) % (this->padding.top + this->padding.bottom + 1 + 1)) == 0 && y != height - 1;
+        for (size_t i = 0; i < this->rows.size(); i++) {
+            const TTableRow& row = this->rows[i];
 
-            size_t col = 0;
-            size_t passedContentWidth = 0;
+            if (prerenderedPaddingTop != L"") {
+                out += prerenderedPaddingTop + L"\n";
+            }
 
-            for (size_t x = 0; x < width; x++) {
-                size_t colStart = 1 + passedContentWidth + (col * (this->padding.left + this->padding.right + 1)) + this->padding.left;
-                size_t offset = x - colStart;
-                bool isContentCol = x >= 1 + this->padding.left && x < width - 1 && col < cols && offset < contentWidths[col];
+            std::wstring content = prerenderedBlank;
+            std::wstring::iterator distPos = content.begin() + 1 + this->padding.left;
 
-                if (col < cols && offset == contentWidths[col]) {
-                    passedContentWidth += contentWidths[col];
-                    col++;
-                }
-
-                if (isContentRow && isContentCol && this->rows[row].cells.size() > col && this->rows[row].cells[col].content.size() > offset) {
-                    out += this->rows[row].cells[col].content[offset];
-                }
-                else {
-                    size_t charIndex = 8;
-                    bool isMergedVertically = y % (this->padding.top + this->padding.bottom + 1 + 1) == 0;
-                    bool isMergedHorizontally = (x - passedContentWidth) == (col * (this->padding.left + this->padding.right + 1));
-
-                    if (y == 0) {
-                        if (x == 0) {
-                            charIndex = 0;
-                        }
-                        else if (x == width - 1) {
-                            charIndex = 2;
-                        }
-                        else if (isMergedHorizontally) {
-                            charIndex = 14;
-                        }
-                        else {
-                            charIndex = 1;
-                        }
-                    }
-                    else if (y == height - 1) {
-                        if (x == 0) {
-                            charIndex = 6;
-                        }
-                        else if (x == width - 1) {
-                            charIndex = 4;
-                        }
-                        else if (isMergedHorizontally) {
-                            charIndex = 15;
-                        }
-                        else {
-                            charIndex = 5;
-                        }
-                    }
-                    else if (x == 0) {
-                        charIndex = 7;
-
-                        if (isMergedVertically) {
-                            charIndex = 12;
-                        }
-                    }
-                    else if (x == width - 1) {
-                        charIndex = 3;
-
-                        if (isMergedVertically) {
-                            charIndex = 13;
-                        }
-                    }
-                    else if (isMergedHorizontally) {
-                        charIndex = 9;
-
-                        if (isMergedVertically) {
-                            charIndex = 11;
-                        }
-                    }
-                    else if (isMergedVertically) {
-                        charIndex = 10;
-                    }
-
-                    out += (*borderCharSet)[charIndex];
+            for (size_t j = 0; j < row.cells.size(); j++) {
+                std::copy(row.cells[j].content.begin(), row.cells[j].content.end(), distPos);
+                
+                if (j != row.cells.size() - 1) {
+                    distPos += contentWidths[j] + this->padding.right + this->padding.left + 1;
                 }
             }
 
-            out += L"\n";
+            out += content + L"\n";
+
+            if (prerenderedPaddingBottom != L"") {
+                out += prerenderedPaddingTop + L"\n";
+            }
+
+            if(i != this->rows.size() - 1) out += prerenderedDivider + L"\n";
         }
+
+        out += prerenderedBottom + L"\n";
 
         return out;
     }
